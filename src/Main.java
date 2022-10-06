18de4a79d3c9dbf8c9312f0c2ef509e802dbd494
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-10-06 22:40:17 +0300

    hw2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 40_000;
+        int rubForBonus = 20;
+
+        System.out.println("Начислено миль:"+(amount / rubForBonus));
+    }
+}
